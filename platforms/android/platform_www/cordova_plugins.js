e11ce329bf35218b91e40bd1bf4643773c94b3d7
Arjun Attam <arjunattam@gmail.com>
2016-11-17 12:57:23 -0800

    Remove plugin and ios platform

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,19 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "id": "cordova-plugin-hypertrack.HyperTrack",
-        "file": "plugins/cordova-plugin-hypertrack/www/HyperTrack.js",
-        "pluginId": "cordova-plugin-hypertrack",
-        "clobbers": [
-            "cordova.plugins.HyperTrack"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.0",
-    "cordova-plugin-hypertrack": "0.1.0"
+    "cordova-plugin-whitelist": "1.3.0"
 };
 // BOTTOM OF METADATA
 });
